791f7b7e80bd33e7331c86282615fd9d2cd76039
fred <alfred_e@outlook.com>
2021-07-02 19:36:52 -0400

    Added a few extras and fixed some code.

--- src/main.c
+++ src/main.c
@@ -2,22 +2,58 @@
 #include <GL/glut.h>
 #include <signal.h>
 #include <time.h>
+#include <math.h>
 #include <../include/binary_matrix.h>
 
 static volatile int keepRunning = 1;
-static BinaryMatrix *M;
+BinaryMatrix *M;
 void intHandler(int dummy)
 {
 	keepRunning = 0;
 }
 
+void drawRect(BinaryMatrix *M, int x, int y, int w, int h) {
+	if (x > M->num_cols || y > M->num_rows 
+	 || x < 0 || y < 0
+	 || w > M->num_cols || h > M->num_rows 
+	 || w < 0 || h < 0) {
+		return;
+	}
+
+	//Draw X axis                     // (x, y)
+	for (int i = 0; i < w; i++) {     // -------------  <-- 1.
+		UpdateEntry(M, y, x+i, 1);    // |           | (x+w, y)
+	}                                 // |           |
+	//Draw Y axis                     // | <-- 2.    |
+	for (int j = 0; j < h; j++) {     // |           | 
+		UpdateEntry(M, y+j, x, 1);    // |     3 --> |
+	}                                 // |           |  (x+w, y+h)
+	for (int k = 0; k < h; k++) {     // -------------  <-- 4
+		UpdateEntry(M, y+k, x+w, 1);  // (x, y+h)
+	}
+	for (int l = 0; l < w; l++) {
+		UpdateEntry(M, y+h, x+l, 1);
+	}
+}
+
+void drawGasket(BinaryMatrix *M, int x, int y, int dimension) {
+	if (dimension <= 8) {
+		drawRect(M, x, y, dimension, dimension);
+	} else {
+		float newDimension = dimension / 2;
+		drawGasket(M, x, y, newDimension);
+		drawGasket(M, x + newDimension, y, newDimension);
+		drawGasket(M, x + newDimension, y + newDimension, newDimension);
+	}
+}
+
 void render() {
 	int i, j, k;
+    glBegin(GL_POINTS);
 	for (i = 0; i < M->num_rows; i++)
 	{
 		for (j = 0; j < M->num_cols; j++)
 		{
-            glBegin(GL_POINTS);
             if (CheckEntry(M,i,j)) {
                 glColor3f(1,1,1);
                 glVertex2i(i,j);
@@ -26,9 +62,9 @@ void render() {
                 glColor3f(0,0,0);
                 glVertex2i(i,j);
             }
-            glEnd();
 		}
 	}
+    glEnd();
     glutSwapBuffers();
 }
 
@@ -122,14 +158,34 @@ int main(int argc, char* argv[]) {
     glLoadIdentity();
     gluOrtho2D( 0.0, 800.0, 600.0,0.0 );
 
-    init();
     struct timespec sleeptime;
 	sleeptime.tv_sec = 0;
 	sleeptime.tv_nsec = 5000000;
-    glutDisplayFunc(render);
-    while (keepRunning) {
-        update();
-        nanosleep(&sleeptime, NULL);
-    }
+	if (argc == 1) {
+        init();
+		glutDisplayFunc(render);
+		while (keepRunning) {
+			update();
+			nanosleep(&sleeptime, NULL);
+		}
+	} else if (argc == 5) {
+			int x = atoi(argv[1]);
+			int y = atoi(argv[2]);
+			int w = atoi(argv[3]);
+			int h = atoi(argv[4]);
+			drawRect(M, x, y, w, h);
+		    glutDisplayFunc(render);
+			render(M);
+			while (keepRunning) {
+				nanosleep(&sleeptime, NULL);
+			}
+	} else {
+		glutDisplayFunc(render);
+		drawGasket(M, 0, 0, 600);
+		render(M);
+		while (keepRunning) {
+			nanosleep(&sleeptime, NULL);
+		}
+	}
 	DeleteBinaryMatrix(M);
 }
