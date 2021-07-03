9c6091f0594c7cdf5df6512d0ec31e88db72f6b1
fred <alfred_e@outlook.com>
2021-07-02 21:17:46 -0400

    Fixed issues found by lgtm

--- src/main.c
+++ src/main.c
@@ -6,55 +6,55 @@
 #include <../include/binary_matrix.h>
 
 static volatile int keepRunning = 1;
-BinaryMatrix *M;
+BinaryMatrix *MATRIX;
 void intHandler(int dummy)
 {
 	keepRunning = 0;
 }
 
-void drawRect(BinaryMatrix *M, int x, int y, int w, int h) {
-	if (x > M->num_cols || y > M->num_rows 
+void drawRect(int x, int y, int w, int h) {
+	if (x > MATRIX->num_cols || y > MATRIX->num_rows 
 	 || x < 0 || y < 0
-	 || w > M->num_cols || h > M->num_rows 
+	 || w > MATRIX->num_cols || h > MATRIX->num_rows 
 	 || w < 0 || h < 0) {
 		return;
 	}
 
-	//Draw X axis                     // (x, y)
-	for (int i = 0; i < w; i++) {     // -------------  <-- 1.
-		UpdateEntry(M, y, x+i, 1);    // |           | (x+w, y)
-	}                                 // |           |
-	//Draw Y axis                     // | <-- 2.    |
-	for (int j = 0; j < h; j++) {     // |           | 
-		UpdateEntry(M, y+j, x, 1);    // |     3 --> |
-	}                                 // |           |  (x+w, y+h)
-	for (int k = 0; k < h; k++) {     // -------------  <-- 4
-		UpdateEntry(M, y+k, x+w, 1);  // (x, y+h)
+	//Draw X axis                          // (x, y)
+	for (int i = 0; i < w; i++) {          // -------------  <-- 1.
+		UpdateEntry(MATRIX, y, x+i, 1);    // |           | (x+w, y)
+	}                                      // |           |
+	//Draw Y axis                          // | <-- 2.    |
+	for (int j = 0; j < h; j++) {          // |           | 
+		UpdateEntry(MATRIX, y+j, x, 1);    // |     3 --> |
+	}                                      // |           |  (x+w, y+h)
+	for (int k = 0; k < h; k++) {          // -------------  <-- 4
+		UpdateEntry(MATRIX, y+k, x+w, 1);  // (x, y+h)
 	}
 	for (int l = 0; l < w; l++) {
-		UpdateEntry(M, y+h, x+l, 1);
+		UpdateEntry(MATRIX, y+h, x+l, 1);
 	}
 }
 
-void drawGasket(BinaryMatrix *M, int x, int y, int dimension) {
+void drawGasket(int x, int y, int dimension) {
 	if (dimension <= 8) {
-		drawRect(M, x, y, dimension, dimension);
+		drawRect(x, y, dimension, dimension);
 	} else {
 		float newDimension = dimension / 2;
-		drawGasket(M, x, y, newDimension);
-		drawGasket(M, x + newDimension, y, newDimension);
-		drawGasket(M, x + newDimension, y + newDimension, newDimension);
+		drawGasket(x, y, newDimension);
+		drawGasket(x + newDimension, y, newDimension);
+		drawGasket(x + newDimension, y + newDimension, newDimension);
 	}
 }
 
 void render() {
 	int i, j, k;
     glBegin(GL_POINTS);
-	for (i = 0; i < M->num_rows; i++)
+	for (i = 0; i < MATRIX->num_rows; i++)
 	{
-		for (j = 0; j < M->num_cols; j++)
+		for (j = 0; j < MATRIX->num_cols; j++)
 		{
-            if (CheckEntry(M,i,j)) {
+            if (CheckEntry(MATRIX,i,j)) {
                 glColor3f(1,1,1);
                 glVertex2i(i,j);
 
@@ -71,8 +71,8 @@ void render() {
 void update() {
 
 	int x, y, z = 0;
-	int width = M->num_cols - 1;
-	int height = M->num_rows - 1;
+	int width = MATRIX->num_cols - 1;
+	int height = MATRIX->num_rows - 1;
 
 	int n = 0;//number of mutations per iteration
 		for (x = 0; x < height; x++)
@@ -80,37 +80,37 @@ void update() {
 			for (y = 0; y < width; y++)
 			{
 				int k = 0;
-				k += CheckEntry(M, x - 1, y - 1);
-				k += CheckEntry(M, x, y - 1);
-				k += CheckEntry(M, x + 1, y - 1);
-				k += CheckEntry(M, x - 1, y);
-				k += CheckEntry(M, x + 1, y);
-				k += CheckEntry(M, x - 1, y + 1);
-				k += CheckEntry(M, x, y + 1);
-				k += CheckEntry(M, x + 1, y + 1);
-				if (CheckEntry(M, x, y))
+				k += CheckEntry(MATRIX, x - 1, y - 1);
+				k += CheckEntry(MATRIX, x, y - 1);
+				k += CheckEntry(MATRIX, x + 1, y - 1);
+				k += CheckEntry(MATRIX, x - 1, y);
+				k += CheckEntry(MATRIX, x + 1, y);
+				k += CheckEntry(MATRIX, x - 1, y + 1);
+				k += CheckEntry(MATRIX, x, y + 1);
+				k += CheckEntry(MATRIX, x + 1, y + 1);
+				if (CheckEntry(MATRIX, x, y))
 				{
  
 					if (k < 2 || k > 3)
 					{
-						UpdateEntry(M, x, y, 0);
+						UpdateEntry(MATRIX, x, y, 0);
 						n++;
 					}
 					else if (k == 2 || k == 3)
 					{
-						UpdateEntry(M, x, y, 1);
+						UpdateEntry(MATRIX, x, y, 1);
 					}
 				}
 				else
 				{
 					if (k == 3)
 					{
-						UpdateEntry(M, x, y, 1);
+						UpdateEntry(MATRIX, x, y, 1);
 						n++;
 					}
 					else
 					{
-						UpdateEntry(M, x, y, 0);
+						UpdateEntry(MATRIX, x, y, 0);
 					}
 				}
 			}
@@ -119,15 +119,15 @@ void update() {
 		{
 	        return;
 		}
-        render(M);
+        render();
 }
 
 void init() {
     time_t t;
     srand((unsigned) time(&t));
 	int x, y, z = 0;
-	int width = M->num_cols - 1;
-	int height = M->num_rows - 1;
+	int width = MATRIX->num_cols - 1;
+	int height = MATRIX->num_rows - 1;
     //Initialize the matrix randomly.
 	for (x = 0; x < width; x++)
 	{
@@ -136,7 +136,7 @@ void init() {
 			int v = (rand() % 100) > 97 ? 1 : 0;
 			if (v)
 			{
-				UpdateEntry(M, y, x, v);
+				UpdateEntry(MATRIX, y, x, v);
 			}
 		}
 	}
@@ -145,7 +145,7 @@ void init() {
 int main(int argc, char* argv[]) {
 
 	signal(SIGINT, intHandler);
-    M = ConstructBinaryMatrix(800,600);
+    MATRIX = ConstructBinaryMatrix(800,600);
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 
@@ -173,19 +173,19 @@ int main(int argc, char* argv[]) {
 			int y = atoi(argv[2]);
 			int w = atoi(argv[3]);
 			int h = atoi(argv[4]);
-			drawRect(M, x, y, w, h);
+			drawRect(x, y, w, h);
 		    glutDisplayFunc(render);
-			render(M);
+			render();
 			while (keepRunning) {
 				nanosleep(&sleeptime, NULL);
 			}
 	} else {
 		glutDisplayFunc(render);
-		drawGasket(M, 0, 0, 600);
-		render(M);
+		drawGasket(0, 0, 600);
+		render();
 		while (keepRunning) {
 			nanosleep(&sleeptime, NULL);
 		}
 	}
-	DeleteBinaryMatrix(M);
+	DeleteBinaryMatrix(MATRIX);
 }
